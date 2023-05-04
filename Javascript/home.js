65d3a50515fe3f6dd79e9809b8a661396262182f
AkselBF <aksel.boutrouefaret@yahoo.com>
2023-05-04 13:17:41 +0200

    updates

--- Javascript/home.js
+++ Javascript/home.js
@@ -1,1 +1,16 @@
-const buttons = document.querySelectorAll();
+//const buttons = document.querySelectorAll("carousel_buttons");
+
+
+
+
+/*
+window.onscroll = function() {slideFunction()};
+
+function slideFunction() {
+  let scrolling = document.querySelector("carousel_dots").scrollTop || document.documentElement.scrollTop;
+  let width = document.documentElement.scrollWidth - document.documentElement.clientWidth;
+
+  let scrolled = (scrolling / width) * 100;
+  document.getElementById("dot_1").style.width = scrolled + "%";
+}
+*/
