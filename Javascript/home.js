145fc7cbff96a4f2017932ab6900634fcef4c74d
AkselBF <aksel.boutrouefaret@yahoo.com>
2023-05-07 18:25:43 +0200

    Update home.js

--- Javascript/home.js
+++ Javascript/home.js
@@ -21,6 +21,29 @@ slides[1].style.left = slideSize * 1 + "px";
 slides[2].style.left = slideSize * 2 + "px";
 */
 
+// Arrange the slides next to one another
+const setSlidePosition = (slide, index) => {
+  slide.style.left = slideSize * index + 'px';
+}
+
+slides.forEach(setSlidePosition);
+
+
+// When clicking left, slides move to the left
+
+
+// When clicking right, slides move to the right
+nextButton.addEventListener('click', e => {
+  const currentSlide = track.querySelector(".current_slide");
+  const nextSlide = currentSlide.nextElementSibling;
+  const amountToMove = nextSlide.style.left;
+
+  track.style.transform = 'TranslateX(-' + amountToMove + ')';
+});
+
+// When clicking the nav indicators, move to that slide
+
+
 // When I click left, move slides to the left
 // When I click right, move slides to the right
 // When I click the dot indicator, move to that slide
