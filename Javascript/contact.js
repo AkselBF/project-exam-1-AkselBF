37483fa694c7b8d266f230c41a34f69c81adddaf
AkselBF <aksel.boutrouefaret@yahoo.com>
2023-05-07 17:48:56 +0200

    updates

--- Javascript/contact.js
+++ Javascript/contact.js
@@ -4,110 +4,107 @@
 */
 
 const form = document.querySelector(".contact_form");
+//const error = document.querySelector(".form_error");
 
 const fullname = document.querySelector("#input_name");
-const fullname_error = document.querySelector("#fullname_error");
+//const fullname_error = document.querySelector("#fullname_error");
 
 const email = document.querySelector("#input_mail");
-const email_error = document.querySelector("#email_error");
+//const email_error = document.querySelector("#email_error");
 
 const subject = document.querySelector("#input_subject");
-const subject_error = document.querySelector("#subject_error");
+//const subject_error = document.querySelector("#subject_error");
 
-const message = document.querySelector("#input_message");
-const message_error = document.querySelector("#message_error");
+const text = document.querySelector("#input_message");
+//const text_error = document.querySelector("#message_error");
 
 const valid = document.querySelector(".message");
 const input = document.querySelector(".input_box");
 const clear_button = document.querySelector(".button_clear");
 const submit_button = document.querySelector(".button_submit");
 
-// Checks if the form passes
-/*
-function validateForm(event) {
-  event.preventDefault();
-}*/
+// Functions
+form.addEventListener("submit", e => {
+  e.preventDefault();
 
-// Checks the full name input
-/*
-if (checkLength(fullname.value, 5) === true) {
-  fullname_error.style.display = "none";
-  console.log(fullname.value);
-} else {
-  fullname_error.style.display = "block";
-}*/
+  validateInputs();
+});
 
-// Checks the e-mail input
-/*
-if (checkMail(email.value) === true) {
-  email_error.style.display = "none";
-  console.log(email.value);
-} else {
-  email_error.style.display = "block";
-}*/
+// Create setError and setSuccess
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".form_error");
 
-// Checks the subject input
-/*
-if (checkLength(subject.value, 15) === true) {
-  subject_error.style.display = "none";
-  console.log(subject.value);
-} else {
-  subject_error.style.display = "block";
-}*/
-
-// Checks the Address input
-/*
-if (checkLength(message.value, 25) === true) {
-  message_error.style.display = "none";
-  console.log(message.value);
-} else {
-  message_error.style.display = "block";
-}*/
-
-// If every input is successfull, a message pops out
-/*
-if (checkLength(fullname.value, 5) === true 
-&& checkMail(email.value) === true 
-&& checkLength(subject.value, 15) === true  
-&& checkLength(message.value, 25) === true) {
-  alert("Submission is successfull");
-  valid.innerHTML = `<h2 class="success_message">Passed</h2>`;
-}*/
-
-//clear_button.disabled = true;
-/*
-submit_button.disabled = true;
-
-input.addEventListener("change", enableButton);
-form.addEventListener("submit", validateForm);
-*/
-
-// If ".input_box" has something in it, the button can be clicked
-/*
-function enableButton() {
-if (document.querySelector(".input_box").value === "") {
-  submit_button.disabled = true;
-  submit_button.style.backgroundColor = "#627b89";
-} else {
-  submit_button.disabled = false;
-  submit_button.style.backgroundColor = "#002A42";
+  errorDisplay.innerText = message;
+  inputControl.classList.add("error");
+  inputControl.classList.remove("success");
 }
-}*/
 
-// Checks the amount of characters
-/*
-function checkLength(value, len) {
-  if (value.trim().length > len) {
-      return true;
+const setSuccess = element => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".form_error");
+
+  errorDisplay.innerText = '';
+  inputControl.classList.add("success");
+  inputControl.classList.remove("error");
+}
+
+const isValidEmail = email => {
+  const regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return regEx.test(String(email).toLowerCase());
+}
+
+// Create form validation
+const validateInputs = () => {
+  const fullnameValue = fullname.value.trim();
+  const emailValue = email.value.trim();
+  const subjectValue = subject.value.trim();
+  const textValue = text.value.trim();
+
+  if (fullnameValue === '') {
+    setError (fullname, "Full name is required");
+  } else if (fullnameValue.length <= 5) {
+    setError (fullname, "Please enter full name (min. 5 characters)")
   } else {
-      return false;
+    setSuccess (fullname);
   }
-}*/
 
-// Checks if the email is properly formulated
+  if (emailValue === '') {
+    setError (email, "Email address is required");
+  } else if (!isValidEmail(emailValue)) {
+    setError (setError, "Please enter valid email address");
+  } else {
+    setSuccess (email);
+  }
+
+  if (subjectValue === '') {
+    setError (subject, "Subject is required");
+  } else if (fullnameValue.length <= 15) {
+    setError (fullname, "Please enter subject (min. 15 characters)")
+  } else {
+    setSuccess (subject);
+  }
+
+  if (textValue === '') {
+    setError (text, "Message is required");
+  } else if (fullnameValue.length <= 25) {
+    setError (fullname, "Please enter message (min. 25 characters)")
+  } else {
+    setSuccess (text);
+  }
+}
+
+
 /*
-function checkMail(email) {
-  const regEx = /\S+@\S+\.\S+/;
-  const patternMatches = regEx.test(email);
-  return patternMatches;
-}*/
+  Regex codes (trials)
+*/
+// Letters and numbers: const regex = /^[a-zA-Z0-9 ]{10,}+$/;
+
+// E-mail: const regEx = /@/;
+// E-mail: const regEx = /\S+@\S+\.\S+/;
+// E-mail: const regEx = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*$/;
+// E-mail: const regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+// Address: const regex = /\w\s\w/;
+// Address: const regex = /^[a-zA-Z0-9 ]{5,}+$/;
